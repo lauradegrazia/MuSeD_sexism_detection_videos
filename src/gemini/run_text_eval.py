6db62ab697f1999791b0cc23f207cc54a1634487
Laura De Grazia <93555610+lauradegrazia@users.noreply.github.com>
2025-04-11 18:15:53 +0200

    Update run_text_eval.py

--- src/gemini/run_text_eval.py
+++ src/gemini/run_text_eval.py
@@ -1,1 +1,89 @@
+import os
+import json
+import argparse
+import pandas as pd
+from gemini_key import api_key
+from google import genai
+from google.genai import types
+from text_utils import (
+    basic_prompt, 
+    basic_prompt_es,
+    basic_prompt_clean_txt,
+    basic_prompt_es_clean_txt,
+    basic_parser, 
+    get_classification_metrics,
+)
+# Documentaation https://ai.google.dev/gemini-api/docs/text-generation?lang=python
+
+client = genai.Client(api_key=api_key)
+
+# Prompts
+prompt_dict = {
+    "basic": basic_prompt,
+    "basic_es": basic_prompt_es,
+    "basic_clean_text": basic_prompt_clean_txt,
+    "basic_es_clean_text": basic_prompt_es_clean_txt
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_path", type=str, help="Path to input file")
+parser.add_argument("--result_path", type=str, help="Path to result file")
+parser.add_argument("--model_name", type=str, default="gemini-2.0-flash", help="Gemini model to run") # gemini-1.5-pro/ gemini-2.0-pro-exp-02-05 / gemini-2.0-flash
+parser.add_argument("--prompt", type=str, default="basic_es_clean_text", help="Prompt type: basic, basic_es, etc.")
+parser.add_argument("--testing", action='store_true', help="Run in testing mode with a sample of 10 rows")
+
+args = parser.parse_args()
+print("Arguments:")
+for arg, value in vars(args).items():
+    print(f"{arg}: {value}")
+
+os.makedirs(args.result_path, exist_ok=True)
+os.makedirs(args.result_path+"/outputs", exist_ok=True)
+
+# Load data
+df = pd.read_csv(args.input_path, header=0)
+if args.testing:
+    df = df.sample(n=15)
+
+# Apply template
+df["prompts"] = df.apply(prompt_dict[args.prompt], axis=1)
+LANG = "es" if "_es" in args.prompt else "en"
+if LANG == "en":
+    sys_instruct = "You are a pattern-following assistant that can only answer with 'Yes' or 'No'. Your goal is to determine whether a text is sexist."
+else:
+    sys_instruct = "Eres un asistente que sigue patrones y solo puede responder con 'Sí' o 'No'. Tu objetivo es determinar si un texto es sexista."
+
+# API calls
+def query_gemini_api(prompt):
+
+    response = client.models.generate_content(
+        config=types.GenerateContentConfig(
+            system_instruction=sys_instruct
+        ),
+        model=args.model_name,
+        contents=[prompt],
+    )
+  
+    return response.text
+
+
+df["output_text"] = df["prompts"].apply(query_gemini_api)
+df["sexist_pred"] = df["output_text"].apply(lambda x: basic_parser(x, language=LANG))
+
+# Save outputs
+df.to_json(f"{args.result_path}/outputs.json", orient="records",indent=4)
+df[["id", "sexist", "sexist_pred"]].to_csv(f"{args.result_path}/outputs.csv", index=False)
+print("Finished saving outputs.")
+
+# Compute metrics
+metrics = get_classification_metrics(df, "sexist", "sexist_pred") 
+
+# Include model name in metrics
+metrics.update({
+    "model": args.model_name,
+})
+
+with open(f"{args.result_path}/metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
+print("Finished saving results.")
 
